ff7cf11f875415ead74ad10300f444f99eca2e97
jjjokpa <jjjokpa13@gmail.com>
2019-10-31 21:07:29 +0900

    Updates

---
+++ precache-manifest.022c2ff27d627033bec391b404eaf6b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d8da0b7770c1d52d1e059fba0283325",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c5d2d22f5551bfbb9f2",
+    "url": "/static/css/main.bb5532bb.chunk.css"
+  },
+  {
+    "revision": "8943fde9ae715f924676",
+    "url": "/static/js/2.cc5af21a.chunk.js"
+  },
+  {
+    "revision": "9c5d2d22f5551bfbb9f2",
+    "url": "/static/js/main.52749e48.chunk.js"
+  },
+  {
+    "revision": "d4e1de6917326fe69011",
+    "url": "/static/js/runtime-main.e3c362c5.js"
+  }
+]);
